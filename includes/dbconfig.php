8b393167ffb4bf58cece5f7827aca6e3eef2aa41
leedahw <71396271+leedahw@users.noreply.github.com>
2020-10-25 17:16:29 -0400

    Revert "2020/10/25"
    This reverts commit 0cde0f109f5a25a05a8fbe837be57a5e4bc675da.

--- includes/dbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php 
-//db-config.php
-$dsn = "mysql:host=localhost;dbname=immnewsnetwork;charset=utf8mb4";
-
-$dbusername = "root";
-$dbpassword = "";
-$pdo = new PDO($dsn, $dbusername, $dbpassword); 
-
-?>
