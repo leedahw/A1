2e990edb3495acd59e561e6d8c51bc3c51a51e3f
leedahw <71396271+leedahw@users.noreply.github.com>
2020-10-26 16:12:34 -0400

    landing page
    added landing.php and logo.png

---
+++ landing.php
@@ -0,0 +1,7 @@
+<?php?>
+
+<a href = "hompage.php"><img src = "logo.png"/></a>
+<br/>
+
+<button type = submit><a href = "login.php">Login</a></button> 
+<button type = submit><a href = "signup.php">Signup</a></button>
