b9cc5c7d0af5c61c9e6cb952d2fc1e494c39ae46
leedahw <71396271+leedahw@users.noreply.github.com>
2020-10-27 19:57:42 -0400

    categorized articles
    add echo function  in feature iframe(line18) in homepage. created three links to article lists categorized.
    created articles-tech/industry/career. not testes articles pgs atm.

---
+++ articles-industry.php
@@ -0,0 +1,33 @@
+<?php session_start();
+if(isset($_SESSION["userId"])){
+    include("includes/standardheader.html");
+
+    //get records from db vv
+include("includes/dbconfig.php");
+
+
+$stmt = $pdo->prepare("SELECT * FROM `article` WHERE `category` = 'tech'");
+
+$stmt->execute();
+while ($row = $stmt->fetch(PDO:: FETCH_ASSOC)){
+    echo("<h4>");
+    echo($row["title"]);
+    echo("</h4>");
+
+    echo("<p>");?>
+    <label>By: </label><?php
+    echo($row["author"]);?><br/>
+    <label>Category: </label><?php
+    echo($row["category"]);?><br/><br/><?php
+    echo($row["content"]);
+    echo("</p>");?>
+
+    <a href = "<?php echo($row["articleLink"]);?>">See Full Article</a><br/>
+<?php
+}
+}else{?>
+    <p>ACCESS DENIED. Please Login</p>
+    <a href = "login.php">Back to Login</a>
+<?php
+}
+?> 
--- homepage.php
+++ homepage.php
@@ -1,40 +1,78 @@
-<?php
-include('includes/standardheader.html');
+<?php 
+//homepage.php
+session_start();
+if(isset($_SESSION["userId"])){
+    include("includes/standardheader.html");
+
+//show the homepage
 ?>
-</head>
-<h1>WELCOME</h1>
+<h1>IMM NEWS NETWORK</h1>
+
 <p>The IMM News Network is a site for students in the Interactive Multimedia Management program at Sheridan College.</p>
-<body>
+
+    <a href = "view-contact.php">View Contact</a>
+
 <br/>
 <br/>
+<h2>Featured Article</h2>
+<iframe src ="<?php echo($row["featured"] == 'yes')?> width="600" height ="400"></iframe>
+<br/>
+
+<h2>Articles</h2>
+<br/>
+    <!-- <a href = "insert-article.php">Add Article</a>
+    <br/>
+    <br/>
+    <nav>
+        <a href = "articles/career1.php">Career Article 1</a>
+        <a href = "articles/career2.php">Career Article 2</a>
+        <a href = "articles/industry1.php">Industry Article 1</a>
+        <a href = "articles/industry2.php">Industry Article 2</a>
+        <a href = "articles/technical1.php">Technical Article 1</a>
+        <a href = "articles/techincal2.php">Technical Article 2</a>
+    </nav> -->
+    <a href = "articles-tech.php">Tech Articles</a>  
+    <a href = "articles-industry.php">Industry Articles</a>  
+    <a href = "articles-career.php">Career Articles</a> 
 <?php
-include('includes/dbconfig.php');
+//get records from db vv
+include("includes/dbconfig.php");
+
 
 $stmt = $pdo->prepare("SELECT * FROM `article`");
 
-$stmt->execute();?>
+$stmt->execute();
+while ($row = $stmt->fetch(PDO:: FETCH_ASSOC)){
+    echo("<h4>");
+    echo($row["title"]);
+    echo("</h4>");
 
-<a href = "insert-article.php">Add Article</a>
+    echo("<p>");?>
+    <label>By: </label><?php
+    echo($row["author"]);?><br/>
+    <label>Category: </label><?php
+    echo($row["category"]);?><br/><br/><?php
+    echo($row["content"]);
+    echo("</p>");?>
 
-<?php
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {    
-echo("<p>");
-        echo($row["articleId"]);?><br/><?php
-        echo($row["title"]);?><br/>
+    <a href = "<?php echo($row["articleLink"]);?>">See Full Article</a><br/>
 
-        ?>
-		<a href="edit-article.php?articleId=<?php echo($row["articleId"]); ?>">Edit</a>
-		<a href="delete-article.php?articleId=<?php echo($row["articleId"]); ?>">Delete</a>
-		<?php
-		echo("</p>");
-}      
+
+	<a href="edit-article.php?articleId=<?php echo($row["articleId"]); ?>">Edit</a>
+	<a href="delete-article.php?articleId=<?php echo($row["articleId"]); ?>">Delete</a><?php
+}
 ?>
-
 </body>
-<br/>
-<a href= "logout.php">Logout<a/>
-<br/>
+
+
 <?php
 include('includes/cookies.html');
+
+ }else{?>
+    <p>ACCESS DENIED. Please Login</p>
+    <a href = "login.php">Back to Login</a>
+<?php
+}
 ?> 
+
 </html>
