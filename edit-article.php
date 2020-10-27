1ba7da931d3ea79ae41d56d41853aab3d9be74e1
leedahw <71396271+leedahw@users.noreply.github.com>
2020-10-27 18:29:06 -0400

    article editing files fixed
     runs succesfully.  changed single quotes to double quotes in include db(line6) in process. double checked to run. changed to "return to Home"(line25) in process.

---
+++ edit-article.php
@@ -0,0 +1,42 @@
+<?php session_start();
+if($_SESSION["userType"]=='admin'){
+
+   $articleId = $_GET["articleId"];
+//connect to db
+	include("includes/dbconfig.php");
+
+    $stmt = $pdo->prepare("SELECT * FROM `article`
+    WHERE `articleId` = $articleId");
+
+    $stmt->execute();
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+// echo($row["title"]);
+// echo($row["author"]);
+// echo($row["content"]);
+// echo($row["articleLink"]);
+
+//show a prefilled form we can edit
+?>
+<form action="process-edit-article.php" method="POST">
+	Title: <input type="text" name="title" value="<?php echo($row["title"]);?>"/><br/>
+    Author: <input type="text" name="author" value="<?php echo($row["author"]);?>"/><br/>
+    <label for= "category">Select Category:</label>
+        <select name="category" id="category">
+            <option value= "technical">technical</option>
+            <option value= "industry">industry</opton>
+            <option value= "career">career</option>
+        </select><br/>
+	Preview: <input type="textarea" size="30" name="content" value="<?php echo($row["content"]);?>"/><br/>
+	Article Link: <input type="text" name="articleLink" value="<?php echo($row["articleLink"]);?>"/>
+	<input type="hidden" name="articleId" value="<?php echo($row["articleId"]);?>">
+	<input type="submit" />
+</form><?php
+}else{
+?>
+<p>ACCESS DENIED. Admin Acess Only</p>
+<a href = "homepage.php">Bcck to Home</a><?php
+
+
+}
+?>
---
+++ process-edit-article.php
@@ -0,0 +1,35 @@
+<?php 
+//insert-edit-article
+session_start();
+if ($_SESSION["userType"]=='admin'){
+//show page
+//receive POST data from edit-article
+$title = $_POST["title"];
+$author = $_POST["author"];
+$category = $_POST["category"];
+$content = $_POST["content"];
+$articleLink = $_POST["articleLink"];
+$articleId = $_POST["articleId"];
+
+//connect to db
+include("includes/dbconfig.php");
+
+//update db with declared var
+$stmt = $pdo->prepare("UPDATE `article` SET `title`='$title' , `author`='$author' , `category`='$category' , `content`='$content' , `articleLink`='$articleLink' 
+WHERE `article` . `articleId` = $articleId");
+
+$stmt->execute();
+?>
+
+<p>Edit Submitted</p>
+<a href = "homepage.php">Back to Home</a>
+<?php
+
+}else{
+?>
+<p>ACCESS DENIED. Admin Access Only.</p>
+<a href = "login.php">Back to Login</a>
+<?php
+
+}
+?>
