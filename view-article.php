3ced1afaa6027d55fdfaaa6b7efd865d142e7943
leedahw <71396271+leedahw@users.noreply.github.com>
2020-10-27 20:17:02 -0400

    new view-article
    made view article so link to article pages can be linked on homepage.php

---
+++ view-article.php
@@ -0,0 +1,47 @@
+<?php 
+//homepage.php
+session_start();
+if(isset($_SESSION["userId"])){
+    include("includes/standardheader.html");
+
+$articleId = $_GET["articleId"];
+//get records from db vv
+include("includes/dbconfig.php");
+
+
+$stmt = $pdo->prepare("SELECT * FROM `article`
+WHERE `articleId` = $articleId");
+
+$stmt->execute();
+
+$row = $stmt->fetch(PDO:: FETCH_ASSOC);
+    echo("<h4>");
+    echo($row["title"]);
+    echo("</h4>");
+    echo("<p>");?>
+    <label>By: </label><?php
+    echo($row["author"]);?><br/>
+    <label>Category: </label><?php
+    echo($row["category"]);?><br/><br/><?php
+    echo($row["content"]);
+    echo("</p>");?>
+
+    <a href = "view-article.php?articleId=<?php echo($row["articleId"]);?>">Read More</a><br/>
+    <a href = "<?php echo($row["articleLink"]);?>">See Full Article</a><br/>
+
+	<a href="edit-article.php?articleId=<?php echo($row["articleId"]); ?>">Edit</a>
+	<a href="delete-article.php?articleId=<?php echo($row["articleId"]); ?>">Delete</a><br/><?php
+
+?>
+
+<?php
+
+
+ }else{?>
+    <p>ACCESS DENIED. Please Login</p>
+    <a href = "login.php">Back to Login</a>
+<?php
+}
+?> 
+
+</html>
